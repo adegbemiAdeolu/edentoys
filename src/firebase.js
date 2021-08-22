2197d5e40bffbd9982e78089155180d06112d9c5
Adegbemi Adeolu <adegbemiadeolu@gmail.com>
2021-08-22 18:36:33 +0100

    ENV

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCsJQpvIqJSuBnGbhEXF2yQYaZcu5zKHEY",
-  authDomain: "eden-toys.firebaseapp.com",
-  databaseURL: "https://eden-toys-default-rtdb.firebaseio.com",
-  projectId: "eden-toys",
-  storageBucket: "eden-toys.appspot.com",
-  messagingSenderId: "1039115557652",
-  appId: "1:1039115557652:web:886742082efef0d58f4001",
-  measurementId: "G-SWMXX83W50"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
